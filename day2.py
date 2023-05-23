f2dc582d246be6afcbff7adb866bd44d9673510a
charlespaul1 <charlespaulwabomba@gmail.com>
2023-05-23 07:38:50 +0300

    checking of a year is leap based on the gregorian calender

---
+++ day2.py
@@ -0,0 +1,19 @@
+# checking if a year is leap using the gregorian calendar
+def is_leap(year):
+    # can evenly be divided by 4
+    if year % 4 == 0:
+        # can evenly be divided by 100
+        if year % 100 == 0:
+            # can evenly be divided by 400
+            if year % 400 == 0:
+                return True
+            else: 
+                return False
+        else:
+            return True
+    else:
+        return False
+    
+    
+print(is_leap(2100))
+        
