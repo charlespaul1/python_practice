956ede5f01acef8abbaf49bb871323e387a74328
charlespaul1 <charlespaulwabomba@gmail.com>
2023-06-25 16:04:16 +0300

    function for prompting player for a move

--- tic-tact-toe.py
+++ tic-tact-toe.py
@@ -7,4 +7,8 @@ def display_board():
     for row in board:
         print('|'.join(row))
         print('-' * 5)
+# function to handle players move/ input
+def players_move():
+    position = input('Enter a position between 1- 9:  ')
+    return int(position)
     
