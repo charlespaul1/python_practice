1a0190cf9062fb16f8fe77f6f58428da93be7a1e
charlespaul1 <charlespaulwabomba@gmail.com>
2023-06-25 13:00:48 +0300

    madlib game

---
+++ madlib.py
@@ -0,0 +1,13 @@
+# creating a madlib
+
+print("Roses are \'    \' ")
+print("violets are \'    \' ")
+print("I love \'    \' ")
+# variables to hold our words
+word1 = input("enter a word: ")
+word2 = input("Enter the second word:    ")
+word3 = input("enter the third word: ")
+print(f"Roses are {word1}")
+print(f"Violets are {word2}")
+print(f" I love {word3}")
+
