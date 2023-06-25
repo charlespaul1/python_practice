dfeab6f5540cbf55463ff0a564ed85e6ca8aecd5
charlespaul1 <charlespaulwabomba@gmail.com>
2023-06-25 15:57:01 +0300

    displaying the board

--- tic-tact-toe.py
+++ tic-tact-toe.py
@@ -2,3 +2,9 @@
 # initializing the board
 board = [[' 'for _ in range(3)] for _ in range(3)]
 
+# displaying the board
+def display_board():
+    for row in board:
+        print('|'.join(row))
+        print('-' * 5)
+    
