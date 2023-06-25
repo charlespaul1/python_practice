d3ed8dde7aee2785cf2c23e46e635f32e1b3ed65
charlespaul1 <charlespaulwabomba@gmail.com>
2023-06-25 16:20:34 +0300

    validating players move on the board

--- tic-tact-toe.py
+++ tic-tact-toe.py
@@ -11,4 +11,19 @@ def display_board():
 def players_move():
     position = input('Enter a position between 1- 9:  ')
     return int(position)
+# function to validate players move on the board
+def validate_move(position):
+    if position < 1 or position > 9:
+        return False
+    # calculating corresponding row and column indices
+    row = (position - 1) // 3
+    column = (position -1) % 3
+    
+    # checking if the corresponding position is empty
+    if board[row][column] != '':
+        return False
+    
+    return True
+    
+
     
